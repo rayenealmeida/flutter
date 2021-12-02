eb73b37d164dc6b91da3b6fc8dc6ac9cad564239
rayene <almeidarayene@gmail.com>
2021-12-02 14:49:15 -0300

    commit

--- widgets/lib/main.dart
+++ widgets/lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:widgets/pages/home_page.dart';
 
 main() {
-  runApp(
-    MaterialApp(home: HomePage()),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
--- widgets/lib/pages/home_page.dart
+++ widgets/lib/pages/home_page.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 200,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(color: Colors.purple[200]),
+        body: Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.purple[200]
+          ),
+          child:
       ),
-      child: Text('Olá Mundo'),
-    );
+     );
+    }
   }
-}
