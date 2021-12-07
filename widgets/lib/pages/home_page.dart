8ea7da0579fbacb92a8d83f7874aa57248b510dc
rayene <almeidarayene@gmail.com>
2021-12-07 14:17:37 -0300

    commit

--- widgets/lib/pages/home_page.dart
+++ widgets/lib/pages/home_page.dart
@@ -11,39 +11,45 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('AppBar'),
       ),
-      body: Column(
+      body: PageView(
         children: [
-          Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(color: Colors.purple[200]),
-              child: Center(
-                child: Text(
-                  'Olá Mundo',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
+          Column(
+            children: [
+              Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(color: Colors.purple[200]),
+                  child: Center(
+                    child: Text(
+                      'Olá Mundo',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                    ),
+                  )),
+              Row(children: [
+                Container(
+                  color: Colors.teal[100],
+                  height: 100,
+                  width: 153,
+                  child: Center(
+                    child: Text('Container 1'),
                   ),
                 ),
-              )),
-          Row(children: [
-            Container(
-              color: Colors.teal[100],
-              height: 100,
-              width: 153,
-              child: Center(
-                child: Text('Container 1'),
-              ),
-            ),
-            Container(
-              color: Colors.pink[100],
-              height: 100,
-              width: 153,
-              child: Center(
-                child: Text('Container 2'),
-              ),
-            ),
-          ])
+                Container(
+                  color: Colors.pink[100],
+                  height: 100,
+                  width: 153,
+                  child: Center(
+                    child: Text('Container 2'),
+                  ),
+                ),
+              ])
+            ],
+          ),
+          Container(color: Colors.red),
+          Container(color: Colors.yellow)
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(items: [
