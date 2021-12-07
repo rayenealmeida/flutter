2b36afa0ded1e06b26c4114b8b3b03fe4d24d308
rayene <almeidarayene@gmail.com>
2021-12-07 14:41:44 -0300

    comit

--- widgets/lib/pages/home_page.dart
+++ widgets/lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   get child => null;
 
