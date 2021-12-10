68dfd5980fdfb653b09e042ccbd7160510b80f80
rayene <almeidarayene@gmail.com>
2021-12-10 17:24:33 -0300

    commit

--- widgets/lib/pages/home_page.dart
+++ widgets/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/pages/pageViews/one_page.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -23,41 +24,7 @@ class _HomePageState extends State<HomePage> {
       body: PageView(
         controller: _pageController,
         children: [
-          Column(
-            children: [
-              Container(
-                  height: 200,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(color: Colors.purple[200]),
-                  child: Center(
-                    child: Text(
-                      'Olá Mundo',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                      ),
-                    ),
-                  )),
-              Row(children: [
-                Container(
-                  color: Colors.teal[100],
-                  height: 100,
-                  width: 150,
-                  child: Center(
-                    child: Text('Container 1'),
-                  ),
-                ),
-                Container(
-                  color: Colors.pink[100],
-                  height: 100,
-                  width: 150,
-                  child: Center(
-                    child: Text('Container 2'),
-                  ),
-                ),
-              ])
-            ],
-          ),
+          OnePage(),
           Container(color: Colors.green[200]),
           Container(color: Colors.yellow[200])
         ],
---
+++ widgets/lib/pages/pageViews/one_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class OnePage extends StatelessWidget {
+  const OnePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.purple[200]),
+            child: Center(
+              child: Text(
+                'Olá Mundo',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            )),
+        Row(
+          children: [
+            Container(
+              color: Colors.teal[100],
+              height: 100,
+              width: 150,
+              child: Center(
+                child: Text('Container 1'),
+              ),
+            ),
+            Container(
+              color: Colors.pink[100],
+              height: 100,
+              width: 150,
+              child: Center(
+                child: Text('Container 2'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
